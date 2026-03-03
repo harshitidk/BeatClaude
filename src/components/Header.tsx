b64001cbffb7b46a145f40b2579639027406a95c
Harshit Kumar Saidaniya <harshitheya@Harshits-MacBook-Air.local>
2026-03-03 18:16:59 +0530

    change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,8 +9,8 @@ export default function Header() {
                 {/* Logo */}
                 <Link href="/" className="flex items-center gap-2.5">
                     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M16 2L6 8V18L16 30L26 18V8L16 2Z" fill="#2563EB" />
-                        <path d="M16 2L6 8V18L16 22V2Z" fill="#1D4ED8" />
+                        <path d="M16 2L6 8V18L16 30L26 18V8L16 2Z" fill="#059669" />
+                        <path d="M16 2L6 8V18L16 22V2Z" fill="#047857" />
                         <path d="M12 14L16 10L20 14L16 22L12 14Z" fill="white" />
                     </svg>
                     <span className="text-lg font-bold text-gray-900 tracking-tight">Beat Claude</span>
